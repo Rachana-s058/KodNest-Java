b93030a34bddedc5fd0daf0b02aa1406243d6a1e
Rachana-s058 <rachanas058@gmail.com>
2026-07-30 12:59:24 +0530

    added one more line

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -6,5 +6,6 @@ public class WelcomeToJava {
         System.out.println("Welcome to java");
         System.out.println("I already know how to build programming logic.");
         System.out.println("Now I am learning how to express that logic using java.");
+        System.out.println("Now i know how to use git and github")
     }
 }
