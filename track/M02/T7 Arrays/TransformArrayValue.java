210456e18fe8bd41abf95d6c41551df2298e52b5
Rachana-s058 <rachanas058@gmail.com>
2026-08-17 22:12:45 +0530

    Day10 pgm

---
+++ track/M02/T7 Arrays/TransformArrayValue.java
@@ -0,0 +1,22 @@
+
+public class TransformArrayValue {
+
+    public static void main(String[] args) {
+        // Write your code here.
+        int[] values = {10, 20, 30, 40, 50};
+        int bonus = 5;
+
+        for (int i = 0; i < values.length; i++) {
+            values[i] = values[i] + bonus;
+        }
+
+        System.out.print("Updated values: ");
+
+        for (int i = 0; i < values.length; i++) {
+            System.out.print(values[i]);
+            if (i < values.length - 1) {
+                System.out.print(" ");
+            }
+        }
+    }
+}
