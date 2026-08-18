f1c13e9b62d4a95c69f8f0e5755a51617242b19d
Rachana-s058 <rachanas058@gmail.com>
2026-08-18 12:32:25 +0530

    Day11 2nd day Array programs

---
+++ track/M02/T7 Arrays/Frequency.java
@@ -0,0 +1,21 @@
+
+public class Frequency {
+
+    int count = 0;
+
+    void count(int arr[], int key) {
+        for (int i = 0; i <= arr.length - 1; i++) {
+            if (arr[i] == key) {
+                count++;
+            }
+        }
+
+        System.out.println(key + ": " + count);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 10, 30, 10, 30};
+        Frequency fs = new Frequency();
+        fs.count(arr, 30);
+    }
+}
