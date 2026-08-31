7703e2070886ca8474ef2d55efb293330d812619
Rachana-s058 <rachanas058@gmail.com>
2026-08-31 15:17:48 +0530

    Day15 Java Array program

---
+++ track/M03 Array-Problem-Solving/CountAverage.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] arr = new int[n];
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
+            sum += arr[i];
+        }
+        double average = (double) sum / n;
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > average) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
